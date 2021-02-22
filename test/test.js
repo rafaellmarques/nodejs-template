9f91758f2f7bb5a14c28b31d5229a4167e51d3d2
Rafael Lourenço Marques <rafaellmarques@outlook.com>
2021-02-22 18:22:03 -0300

    Husky & Lint-Staged

--- test/test.js
+++ test/test.js
@@ -1,6 +1,11 @@
 const number = '2';
-console.info(`numero: ${number}`);
+console.log(`numero: ${number}`);
 
-if (number == 2 && number === 2) {
-    console.info(number);
+if (number === 2) {
+    console.log(number);
+}
+
+if(number == 3 && number > 100) {
+    console.log("data 3");
+    console.log("data z:" + number);
 }
