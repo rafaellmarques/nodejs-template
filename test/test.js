2f2bd7b6da8fb2601f72827b15cd38e0f6077fbe
Rafael Lourenço Marques <rafaellmarques@outlook.com>
2021-02-22 13:31:26 -0300

    Configuração do prettier

--- test/test.js
+++ test/test.js
@@ -1,2 +1,14 @@
 const number = 2;
 console.log('numero: ' + number);
+
+if (
+    number == 2 &&
+    number == 2 &&
+    number == 2 &&
+    number == 2 &&
+    number == 2 &&
+    number == 2 &&
+    number == 2
+) {
+    console.log(number);
+}
